5c2e55189929c34b2b82828b875ad2a6c14e53e7
electricface <songwentai@deepin.com>
2019-04-11 12:42:26 +0800

    release 不用 upx 压缩

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,6 @@ rm -rf build
 mkdir build
 cd build
 go build -o pr-test -ldflags='-s -w' github.com/electricface/deepin-pr-test/cmd/pr-test
-upx pr-test
-upx -t pr-test
 ./pr-test --help || echo
 tar -cJf pr-test.tar.xz pr-test
 ls -lh pr-test.tar.xz
